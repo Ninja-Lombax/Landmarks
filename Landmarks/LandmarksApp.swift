3429e1ce4ea623d6a0557a7e963f2188f1733eaf
ninjalombax <godzillakotm80@gmail.com>
2021-02-13 12:11:02 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by John Grasser on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
